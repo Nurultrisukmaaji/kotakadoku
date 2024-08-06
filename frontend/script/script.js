59940653248e809eddc7eb5dd9daa152035c9746
Nurul Tri Sukma Aji <Nurultrisukmaaji@gmail.com>
2024-08-06 08:17:08 +0700

    Kotakadoku commit

---
+++ frontend/script/script.js
@@ -0,0 +1,20 @@
+const cont = document.querySelector('.gallery');
+const jumbo = document.querySelector('.jumbo');
+const thumb = document.querySelector('.thumb');
+
+
+cont.addEventListener('click', function(e){
+    if (e.target.className='thumb'){
+        jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500);
+
+        thumb.forEach(function(thumb){
+            thumb.className='thumb';
+        });
+
+        e.target.classList.add('active-js');
+    }
+});
